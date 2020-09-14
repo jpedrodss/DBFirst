bb878fb74db2208d83b4a033be774b979bdf5371
João Pedro <jpedrodss@gmail.com>
2020-09-14 12:33:32 -0300

    update

---
+++ Repositories/ProdutoRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataBaseFirst.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataBaseFirst.Repositories
+{
+    public class ProdutoRepository
+    {
+        private readonly PedidoContext _ctx;
+
+        public ProdutoRepository()
+        {
+            _ctx = new PedidoContext();
+            _ctx.Pedidos
+        }
+    }
+}
